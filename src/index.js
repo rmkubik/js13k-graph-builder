e659ee87a00a30064408c249f6cc9383093b8fdf
Ryan Kubik <rkubik@twilio.com>
2020-09-28 16:02:55 -0700

    Ouput commits build size outputs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-const {
-  argv
-} = require("yargs")
+const { argv } = require("yargs")
   .option("zipPath", {
     alias: "path",
     type: "string",
@@ -18,27 +16,41 @@ const {
     type: "string",
     description: "Path to your project's root.",
     required: true,
+  })
+  .option("outputDirectory", {
+    alias: "out",
+    type: "string",
+    description: "Location of output data file.",
+    default: process.cwd(),
+  })
+  .option("commitLimit", {
+    alias: "limit",
+    type: "number",
+    description:
+      "How many previous commits to calculate. Use 0 to show all commits.",
+    default: 1,
   });
 
 require("intl");
 
 const shell = require("shelljs");
 const fs = require("fs");
+const { promisify } = require("util");
+const writeFile = promisify(fs.writeFile);
 const path = require("path");
-const chalk = require('chalk');
-const {
-  stripIndents
-} = require('common-tags')
+const chalk = require("chalk");
+const { stripIndents } = require("common-tags");
+const simpleGit = require("simple-git");
 
 async function buildProject(buildCommand) {
-  // asbstract this into an array of commands the user wants to run?
+  // TODO: allow arbitrary array of commands via a config file
 
   // clean node_modules
-  const nodeModulesPath = 'node_modules';
+  const nodeModulesPath = "node_modules";
 
   if (fs.existsSync(nodeModulesPath)) {
     console.log(
-      stripIndents(chalk `
+      stripIndents(chalk`
         {blue {bold Cleaning installation directory}}
         {blue Command: } rm -rf ${nodeModulesPath}
       `)
@@ -48,7 +60,7 @@ async function buildProject(buildCommand) {
 
   // do npm install
   console.log(
-    stripIndents(chalk `
+    stripIndents(chalk`
       {blue {bold Installing dependencies}}
       {blue Command: } npm install
     `)
@@ -57,7 +69,7 @@ async function buildProject(buildCommand) {
 
   // run build command
   console.log(
-    stripIndents(chalk `
+    stripIndents(chalk`
       {blue {bold Building project}}
       {blue Command: } ${buildCommand}
     `)
@@ -71,19 +83,68 @@ async function evaluteBuildSize(zipPath) {
   return stats.size;
 }
 
+async function getCommitList() {
+  if (!shell.which("git")) {
+    shell.echo("Sorry, this script requires git");
+    shell.exit(1);
+  }
+
+  const git = simpleGit({
+    baseDir: shell.pwd().stdout,
+  });
+
+  // TODO: allow specification of commits via a config file
+  const commits = await git.log();
+
+  return commits.all.map((commit) => commit.hash);
+}
+
+async function checkoutCommit(commit) {
+  const git = simpleGit({
+    baseDir: shell.pwd().stdout,
+  });
+
+  await git.checkout(commit);
+}
+
+async function writeOutput(outputDirectory, output) {
+  const outputFilePath = path.join(outputDirectory, "output.json");
+
+  await writeFile(outputFilePath, JSON.stringify(output, undefined, 2));
+}
+
 async function run() {
   const {
     zipPath,
     buildCommand,
-    projectDirectory
+    projectDirectory,
+    outputDirectory, // default to cwd for output
+    commitLimit,
   } = argv;
 
   shell.cd(projectDirectory);
 
-  await buildProject(buildCommand);
+  const commits = await getCommitList();
 
-  const buildSize = await evaluteBuildSize(zipPath);
-  console.log(buildSize);
+  // limit amount of commits used in output
+  // commitLimit 0 means show all commits
+  if (commitLimit !== 0) {
+    commits.splice(commitLimit);
+  }
+
+  const output = [];
+
+  for (commit of commits) {
+    await checkoutCommit(commit);
+
+    await buildProject(buildCommand);
+
+    const buildSize = await evaluteBuildSize(zipPath);
+
+    output.push({ commit, buildSize });
+  }
+
+  await writeOutput(outputDirectory, output);
 }
 
 run();
