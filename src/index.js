4becc51a28ac5d44811787c37809adcf97dae788
Ryan Kubik <rkubik@twilio.com>
2020-09-28 13:56:47 -0700

    Log more info

--- src/index.js
+++ src/index.js
@@ -1,68 +1,89 @@
 const {
-    argv
+  argv
 } = require("yargs")
-    .option("zipPath", {
-        alias: "path",
-        type: "string",
-        description: "Path to your zipped build, relative to project root.",
-        required: true,
-    })
-    .option("buildCommand", {
-        alias: "cmd",
-        type: "string",
-        description: "Command to execute to build your zip file.",
-        required: true,
-    })
-    .option("projectDirectory", {
-        alias: "dir",
-        type: "string",
-        description: "Path to your project's root.",
-        required: true,
-    });
+  .option("zipPath", {
+    alias: "path",
+    type: "string",
+    description: "Path to your zipped build, relative to project root.",
+    required: true,
+  })
+  .option("buildCommand", {
+    alias: "cmd",
+    type: "string",
+    description: "Command to execute to build your zip file.",
+    required: true,
+  })
+  .option("projectDirectory", {
+    alias: "dir",
+    type: "string",
+    description: "Path to your project's root.",
+    required: true,
+  });
 
 require("intl");
 
 const shell = require("shelljs");
 const fs = require("fs");
 const path = require("path");
-// const {
-//     stripIndents
-// } = require("common-tags");
+const chalk = require('chalk');
+const {
+  stripIndents
+} = require('common-tags')
 
 async function buildProject(buildCommand) {
-    // clean node_modules
-    const nodeModulesPath = 'node_modules';
+  // asbstract this into an array of commands the user wants to run?
 
-    if (fs.existsSync(nodeModulesPath)) {
-        shell.rm("-rf", nodeModulesPath);
-    }
+  // clean node_modules
+  const nodeModulesPath = 'node_modules';
 
-    // do npm install
-    shell.exec("npm install");
+  if (fs.existsSync(nodeModulesPath)) {
+    console.log(
+      stripIndents(chalk `
+        {blue {bold Cleaning installation directory}}
+        {blue Command: } rm -rf ${nodeModulesPath}
+      `)
+    );
+    shell.rm("-rf", nodeModulesPath);
+  }
 
-    // run build command
-    shell.exec(buildCommand);
+  // do npm install
+  console.log(
+    stripIndents(chalk `
+      {blue {bold Installing dependencies}}
+      {blue Command: } npm install
+    `)
+  );
+  shell.exec("npm install");
+
+  // run build command
+  console.log(
+    stripIndents(chalk `
+      {blue {bold Building project}}
+      {blue Command: } ${buildCommand}
+    `)
+  );
+  shell.exec(buildCommand);
 }
 
 async function evaluteBuildSize(zipPath) {
-    const stats = fs.statSync(zipPath);
+  const stats = fs.statSync(zipPath);
 
-    return stats.size;
+  return stats.size;
 }
 
 async function run() {
-    const {
-        zipPath,
-        buildCommand,
-        projectDirectory
-    } = argv;
+  const {
+    zipPath,
+    buildCommand,
+    projectDirectory
+  } = argv;
 
-    shell.cd(projectDirectory);
+  shell.cd(projectDirectory);
 
-    await buildProject(buildCommand);
+  await buildProject(buildCommand);
 
-    const buildSize = await evaluteBuildSize(zipPath);
-    console.log(buildSize);
+  const buildSize = await evaluteBuildSize(zipPath);
+  console.log(buildSize);
 }
 
 run();
